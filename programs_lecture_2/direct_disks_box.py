23f316d50309bb2afa9702d2eb4ae728351d3cb7
Raman Ganti <topraman33@gmail.com>
2014-04-02 17:14:30 +0100

    added comments to clarify code

--- programs_lecture_2/direct_disks_box.py
+++ programs_lecture_2/direct_disks_box.py
@@ -4,14 +4,14 @@ N = 4
 sigma = 0.2
 condition = False
 while condition == False:
-    L = [(random.uniform(sigma, 1.0 - sigma), random.uniform(sigma, 1.0 - sigma))]
+    L = [(random.uniform(sigma, 1.0 - sigma), random.uniform(sigma, 1.0 - sigma))] # Generates random x,y coordinate
     for k in range(1, N):
         a = (random.uniform(sigma, 1.0 - sigma), random.uniform(sigma, 1.0 - sigma))
-        min_dist = min(math.sqrt((a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2) for b in L) 
-        if min_dist < 2.0 * sigma: 
+        min_dist = min(math.sqrt((a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2) for b in L) # Loops through L to check dist for e/ hard disk
+        if min_dist < 2.0 * sigma: #if there's overlap of disks
             condition = False
-            break
+            break # start from beginning of while loop due to overlap
         else:
-            L.append(a)
+            L.append(a) # adds the new circle coordinates to L
             condition = True
 print L
--- programs_lecture_2/markov_disks_box.py
+++ programs_lecture_2/markov_disks_box.py
@@ -6,10 +6,10 @@ sigma_sq = sigma ** 2
 delta = 0.1
 n_steps = 1000
 for steps in range(n_steps):
-    a = random.choice(L)
+    a = random.choice(L) # probably picks a random circle in L
     b = [a[0] + random.uniform(-delta, delta), a[1] + random.uniform(-delta, delta)]
     min_dist = min((b[0] - c[0]) ** 2 + (b[1] - c[1]) ** 2 for c in L if c != a)
-    box_cond = min(b[0], b[1]) < sigma or max(b[0], b[1]) > 1.0 - sigma
+    box_cond = min(b[0], b[1]) < sigma or max(b[0], b[1]) > 1.0 - sigma #if this isn't satisfied, means circle touches or overlaps the wall
     if not (box_cond or min_dist < 4.0 * sigma ** 2):
         a[:] = b
 print L
